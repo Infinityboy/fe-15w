97ad5d7afef4f8d93430dba378363cd0def2a642
liangqi <liangqi000@gmail.com>
2016-05-17 21:34:29 +0800

    upade code

--- js/detail-game.js
+++ js/detail-game.js
@@ -4,7 +4,7 @@ window.Jn = {};
 var cacheData = null;
 
 Jn.setData = function (data) {
-    if (data.key == 'videoInitDetail') {
+    if (data.key == 'gameInitDetail') {
         renderData(data.content);
     }
 };
@@ -21,9 +21,14 @@ function renderData(data) {
     htmlStr += '<span class="header-teams-left">' + data.teamA.name + '</span> ';
     htmlStr += '<div class="header-teams-middle"><span>' + data.gameTime + '</span></div> ';
     htmlStr += '<span class="header-teams-right">' + data.teamB.name + '</span> </section> <section class="ProgressBar clearFix"> ';
-    htmlStr += '<div class="ProgressBar-leftbox"><div class="ProgressBar-left"></div></div> <div class="ProgressBar-tag">';
+
+    // 单元长度为15px
+    var leftItemLength = data.teamA.score * 15;
+    var rightItemLength = data.teamB.score * 15;
+
+    htmlStr += '<div class="ProgressBar-leftbox"><div class="ProgressBar-left" style="width:' + leftItemLength + 'px"></div></div> <div class="ProgressBar-tag">';
     htmlStr += '<img src="images/matchdetail_image_redbule.png"/></div>';
-    htmlStr += '<div class="ProgressBar-rightbox"><div class="ProgressBar-right"></div></div> </section>';
+    htmlStr += '<div class="ProgressBar-rightbox"><div class="ProgressBar-right" style="width:' + rightItemLength + '"></div></div> </section>';
     htmlStr += '<section class="supporters"> <img class="supporters-leftlogo" ';
     htmlStr += 'src="images/matchdetail_ic_support.png"/> ';
     htmlStr += '<p class="supporters-leftNumber supp"id="supporters-leftNumber">' + data.teamA.support_numbber + '</p>';
@@ -39,7 +44,7 @@ function renderData(data) {
         htmlStr += '<section class="live-byword-listbox"> <ul>';
         $.each(data.relateArticle, function (index, item) {
             htmlStr += '<li> <div class="innerbox"><div class="live-byword-list .clearFix"> ';
-            htmlStr += '<a href="" class="live-byword-listimg"><img src="' + item.avatar + '"/> </a>';
+            htmlStr += '<a href="##" class="live-byword-listimg"><img src="' + item.avatar + '"/> </a>';
             htmlStr += '<div class="fl live-byword-list-games" > ';
             htmlStr += '<p class="live-byword-list-name">' + item.author + '</p> ';
             htmlStr += '<p class="live-byword-list-time">' + item.round + '<span>' + '&nbsp;' + '' + item.time + '</span></p></div></div> ';
@@ -60,28 +65,30 @@ function renderData(data) {
         htmlStr += '</section> <section class="header-bottomline2"></section>';
     }
 
-    //比赛视频
+    //相关视频
     if (data.relateVideos.length > 0) {
         htmlStr += '<section class="gamelist"><h2>比赛视频</h2> <ul> ';
 
         $.each(data.relateVideos, function (index, item) {
-            htmlStr += '<li class="clearFix video-recommend" data-type="'+item.articleType+'" data-id="'+item.extra+'"> <img class="fl" src="' + item.thumbnail + '"/>';
+            htmlStr += '<li class="clearFix video-recommend" data-type="' + item.articleType + '" data-id="' + item.extra + '"> <img class="fl" src="' + item.thumbnail + '"/>';
             htmlStr += '<p class="gamelist-into-title">' + item.title + '</p> ';
             htmlStr += '<p class="gamelist-into-text">' + item.excerpt + '</p>';
             htmlStr += '<a style="color:' + item.tagColor + ';border-color:' + item.tagColor + ';">' + item.tagName + '</a></li>';
         });
 
-        htmlStr += '</ul> </section><section class="header-bottomline2"></section> <section class="gamevedio"> ';
+        htmlStr += '</ul></section><section class="header-bottomline2"></section>';
+    } else {
+        htmlStr += '<section class="header-bottomline2"></section><section class="gamevedio"> ';
         htmlStr += '<h2>比赛视频</h2><img src="images/gamevedio.png">';
-        htmlStr += ' <p>LOL春季赛常规赛RNG VS Snake 第三场直播</p></section> ';
-        htmlStr += '<section class="header-bottomline2"></section>';
+        htmlStr += '<p>LOL春季赛常规赛RNG VS Snake 第三场直播</p></section> ';
+        htmlStr += '</section><section class="header-bottomline2"></section>';
     }
 
-    //相关新闻
+    //推荐视频
     if (data.recomendVideos.length > 0) {
         htmlStr += '<section class="gamenews"> <h2>相关新闻</h2><ul>';
         $.each(data.recomendVideos, function (index, item) {
-            htmlStr += '<li class="clearFix related-news" data-type="'+item.articleType+'" data-id="'+item.extra+'"><div class="relanews">';
+            htmlStr += '<li class="clearFix related-news" data-type="' + item.articleType + '" data-id="' + item.extra + '"><div class="relanews clearFix">';
             htmlStr += '<img class="fl" src="' + item.thumbnail + '"/>';
             htmlStr += '<p class="gamenews-into-title">' + item.title + '</p>';
             htmlStr += '<p class="gamenews-into-text">' + item.excerpt + '</p>';
@@ -102,115 +109,140 @@ function renderData(data) {
         var itemWidth = screen / len;
 
         $.each(data.gameContent, function (index, item) {
-            htmlStr += '<div class="roundspan3" style="width:' + itemWidth + 'px" >' + item.title + '</div>';
-
+            if (index == 0) {
+                htmlStr += '<a href="##" class="roundspan3 on" data-id="' + index + '" style="width:' + itemWidth + 'px" >' + item.title + '</a>';
+            } else {
+                htmlStr += '<a href="##" class="roundspan3" data-id="' + index + '" style="width:' + itemWidth + 'px" >' + item.title + '</a>';
+            }
         });
 
         htmlStr += '</div>';
-        //$('#roundlist span').css('background','red');
+
+
+        htmlStr += '<div class="data-details">';
 
         $.each(data.gameContent, function (index, item) {
-            htmlStr += '<div class="data-details" style="display:none;"> ' +
-                '<section class="gamedata-teams"> <img class="gamedata-teams-logo1eft" ' +
-                'src="images/game-logo1.png"/> ' +
+            if (index == 0) {
+                htmlStr += '<div class="data-content-item">';
+            } else {
+                htmlStr += '<div class="data-content-item" style="display: none">';
+            }
+            htmlStr += '<section class="gamedata-teams"><img class="gamedata-teams-logo1eft" ' + 'src="' + data.teamA.logo + '"/> ' +
                 '<p class="gamedata-teams-duration">' + data.gameContent[index].time + '</p> ' +
-                '<img class="gamedata-teams-logoright" src="images/game-logo2.png"/></section> ' +
-                '<section class="gamedata-select clearFix"> <div class="fl clearFix">';
+                '<img class="gamedata-teams-logoright" src="' + data.teamB.logo + '"/></section> ';
 
-            var gamecontentindex = data.gameContent[index].dataList.list;
-            $.each(gamecontentindex[0].fieldData[0], function (index, item) {
-                htmlStr += '<img class="gamedata-select-logo1eft" src="' + item + '"/>';
+            var itemList = item.dataList.list;
+
+            $.each(itemList, function (index, subItem) {
+                if (subItem.listType == 0) {
+                    htmlStr += '<section class="game-data-select">';
+
+                    htmlStr += '<div class="img-left">';
+                    var leftArr = subItem.fieldData[0];
+                    $.each(leftArr, function (index, imgItem) {
+                        htmlStr += '<img class="game-data-select-logo1eft" src="' + imgItem + '"/>';
+                    });
+                    htmlStr += '</div>';
+
+                    htmlStr += '<p class="game-data-select-word">' + itemList[0].fieldName + '</p>';
+
+                    var rightArr = subItem.fieldData[1];
+                    htmlStr += '<div class="img-right">';
+                    $.each(rightArr, function (index, imgItem) {
+                        htmlStr += '<img class="game-data-select-logo1eft" src="' + imgItem + '"/>';
+                    });
+                    htmlStr += '</div>';
+                    htmlStr += ' </section>';
+
+                } else if (subItem.listType == 1) {
+                    // 计算单元长度
+                    var leftBarLength = parseInt(subItem.fieldData[0]);
+                    var rightBarLength = parseInt(subItem.fieldData[1]);
+
+                    var leftBarWidth, rightBarWidth;
+                    if (leftBarLength + rightBarLength > 100) {
+                        leftBarWidth = leftBarLength * 100 / (leftBarLength + rightBarLength);
+                        rightBarWidth = rightBarLength * 100 / (leftBarLength + rightBarLength)
+                    } else {
+                        leftBarWidth = leftBarLength;
+                        rightBarWidth = rightBarLength
+                    }
+
+                    htmlStr += '<section class="conpares clearFix">';
+                    htmlStr += '<div class="compares-bardetails clearFix">'
+                    htmlStr += '<div class="earnmoney">' + subItem.fieldName + '</div>';
+                    htmlStr += '<div class="compares-barleft" style="width:' + leftBarWidth + 'px"><span class="compares-bardetails-leftnum">' + subItem.fieldData[0] + '</span> </div>';
+                    htmlStr += '<div class="compares-barmiddle"><img src="images/matchdetail_image_redbule.png"/></div>';
+                    htmlStr += '<div class="compares-barright" style="width:' + rightBarWidth + 'px"><span class="compares-bardetails-rightnum">' + subItem.fieldData[1] + '</span></div>';
+                    htmlStr += '</div>';
+                    htmlStr += '</section>'
+                }
             });
 
-            htmlStr += '</div> <p class="gamedata-select-word">' + gamecontentindex[0].fieldName + '</p> <div class="fr clearFix">';
-            $.each(gamecontentindex[0].fieldData[1], function (index, item) {
-                htmlStr += '<img class="gamedata-select-logoright" src="' + item + '"/>';
-            });
-            htmlStr += ' </div></section> ';
-            htmlStr += '<section class="conpares clearFix">' +
-                ' <div class="compares-bardetails clearFix"><div class="earnmoney">经济</div>' +
-                ' <div class="compares-barleft"> ' +
-                '<span class="compares-bardetails-leftnum">' + gamecontentindex[1].fieldData[0] + 'k</span> </div> ' +
-                '<div class="compares-barmiddle"> ' +
-                '<img src="images/matchdetail_image_redbule.png"/> </div> ' +
-                '<div class="compares-barright"> ' +
-                '<span class="compares-bardetails-rightnum">' + gamecontentindex[1].fieldData[1] + 'k</span> </div> </div></section>';
             htmlStr += '</div>';
 
         });
-        htmlStr += '</section>';
+        htmlStr += '</div></section>';
     }
     htmlStr += '<section class="header-bottomline2"></section> ';
-
     $('#box').html(htmlStr);
 
-    //点赞
-    $('.supporters').on('click', 'img', function (e) {
-        if (getCookie('num')) {
-            return;
+}
+
+$(function () {
+    document.documentElement.style.fontSize = document.documentElement.clientWidth / 320 * 20 + 'px';
+
+    $.get('data/game-detail.json', function (res) {
+        if (res.code == 10000) {
+            renderData(res.data);
         }
-        var thisindex = parseInt($(this).index());
-        if (thisindex == 0) {
-            $(this).attr('src', 'images/matchdetail_ic_support_red.png');
-            var oldValue = parseInt($('#supporters-leftNumber').html());
-            oldValue++;
-            $('#supporters-leftNumber').html(oldValue);
-            try {
-                Jnapp.agree(7,cacheData.dataId,cacheData.teamA.teamId);
-            } catch (e){
-
-            }
-            setCookie('num', '1', 1);
-        } else {
-            $(this).attr('src', 'images/matchdetail_ic_support_blue.png');
-            var oldValue = parseInt($('#supporters-rightNumber').html());
-            oldValue++;
-            $('#supporters-rightNumber').html(oldValue);
-            setCookie('num', '1', 1);
-
-            try {
-                Jnapp.agree(7,cacheData.dataId,cacheData.teamB.teamId);
-            } catch (e){
-
-            }
-        }
-
-        e.preventDefault();
-
     });
 
 
-    //header得分比例
+    $(document).on('click', '.video-recommend, .related-news', function (e) {
+        e.preventDefault();
+        try {
+            Jnapp.jn_related($(this).data('type'), $(this).data('id'));
+        } catch (e) {
 
-    var iScoreA = $(".header-scores-left b").text();
-    var iScoreB = $(".header-scores-right b").text();
-    var iScoretot = parseInt(iScoreA) + parseInt(iScoreB) * 3;
-    var iScaleA = iScoreA / iScoretot;
-    var iScaleB = iScoreB / iScoretot;
-    var oBarlength = parseInt($(".ProgressBar").width()) - parseInt($(".ProgressBar-tag").css("width"));
-    var oProgressBarleft = $(".ProgressBar-left");
-    var oProgressBarright = $(".ProgressBar-right");
+        }
+    });
 
-    oProgressBarleft.css("width", iScaleA * oBarlength);
-    oProgressBarright.css("width", iScaleB * oBarlength);
+    //left 点赞支持
+    $(document).on('click', '.supporters-leftlogo, .supporters-leftNumber', function () {
+        var elem = $('#supporters-leftNumber');
+        var number = parseInt(elem.html());
+        //if (getCookie("sp-" + cacheData.dataId)) {
+        //    return;
+        //}
+        try {
+            number++;
+            elem.html(number);
+            //setCookie("sp-" + cacheData.dataId, number, 365);
+            //Jnapp.jn_agree(7, cacheData.dataId, cacheData.teamB.teamId);
+            Jnapp.jn_agree(7, 1212, 12);
+        } catch (e) {
 
-    //比赛数据比例  设置页面加载默认值
+        }
+    });
 
-    var iMoneyA = $(".compares-bardetails-leftnum").eq(0).text();
-    var iMoneyB = $(".compares-bardetails-rightnum").eq(0).text();
-    var iMoneytot = parseFloat(iMoneyA) + parseFloat(iMoneyB);
+    // right 点赞支持
+    $(document).on('click', '.supporters-rightlogo, .supporters-rightNumber', function () {
+        var elem = $('#supporters-rightNumber');
+        var number = parseInt(elem.html());
+        //if (getCookie("sp-" + cacheData.dataId)) {
+        //    return;
+        //}
+        try {
+            number++;
+            elem.html(number);
+            //setCookie("sp-" + cacheData.dataId, number, 365);
+            Jnapp.jn_agree(7, cacheData.dataId, cacheData.teamB.teamId);
+        } catch (e) {
+            alert('')
+        }
+    });
 
-    var iMoneyscaleA = parseFloat(iMoneyA) / iMoneytot;
-    var iMoneyscaleB = parseFloat(iMoneyB) / iMoneytot;
-    var oMoneylength = parseInt($('.compares-bardetails').css('width'));
-    var oMoneyleft = $(".compares-barleft").eq(0);
-    var oRightright = $(".compares-barright").eq(0);
-
-    oMoneyleft.css("width", iMoneyscaleA * oMoneylength);
-    oRightright.css("width", iMoneyscaleB * oMoneylength);
-
-    $('.roundspan3').eq(0).addClass('on');
-    $('.data-details').eq(0).show();
 
     //图文直播
     $('.live-byword-listbox ul').on('click', 'li', function (e) {
@@ -218,82 +250,14 @@ function renderData(data) {
         e.preventDefault();
 
     });
-    //推荐视频
-    $('.gamelist ul').on('click', 'li', function (e) {
-        console.log($(this).index());
+
+
+    $(document).on('click', 'a.roundspan3', function (e) {
         e.preventDefault();
-
-    });
-    //比赛视频
-    $('document').on('click', '.gamevedio', function (e) {
-        console.log(110);
-        e.preventDefault();
-
-    });
-    //相关新闻
-    $('.gamenews ul').on('click', 'li', function (e) {
-        console.log($(this).index());
-        e.preventDefault();
-    });
-}
-window.onload = window.onresize = function () {
-    document.documentElement.style.fontSize = document.documentElement.clientWidth / 320 * 20 + 'px';
-
-    /*$(function () {
-        $.get('data/game-detail.json', function (res) {
-            if (res.code == 10000) {
-                renderData(res.data);
-            }
-        });
-    });*/
-
-    $('body').on('click', '#roundlist div', function (e) {
-        $('#roundlist div').removeClass('on');
-        $(this).addClass('on');
-        $('.data-details').hide();
-        $('.data-details').eq($(this).index()).show();
-
-        var iMoneyA = $(".compares-bardetails-leftnum").eq($(this).index()).text();
-        var iMoneyB = $(".compares-bardetails-rightnum").eq($(this).index()).text();
-        var iMoneytot = parseFloat(iMoneyA) + parseFloat(iMoneyB);
-
-        var iMoneyscaleA = parseFloat(iMoneyA) / iMoneytot;
-        var iMoneyscaleB = parseFloat(iMoneyB) / iMoneytot;
-        var oMoneylength = parseInt($('.compares-bardetails').css('width'));
-        var oMoneyleft = $(".compares-barleft").eq($(this).index());
-        var oRightright = $(".compares-barright").eq($(this).index());
-
-        oMoneyleft.css("width", iMoneyscaleA * oMoneylength * 0.27);
-        oRightright.css("width", iMoneyscaleB * oMoneylength * 0.27);
-
-
-        e.preventDefault();
-    });
-
-
-    $(document).on('click','.video-recommend', function(e){
-        e.preventDefault();
-        try{
-            Jnapp.jn_related($(this).data('type'), $(this).data('id'));
-        }catch(e){
-
+        if (!$(this).hasClass('on')) {
+            var index = $(this).data('id');
+            $(this).addClass('on').siblings('a').removeClass('on');
+            $('.data-details > .data-content-item').eq(index).show().siblings().hide();
         }
-        console.log($(this).data('type'));
-        console.log($(this).data('id'));
-
     });
-
-    $(document).on('click','.related-news', function(e){
-        e.preventDefault();
-        try{
-            Jnapp.jn_related($(this).data('type'), $(this).data('id'));
-        }catch(e){
-
-        }
-        console.log($(this).data('type'));
-        console.log($(this).data('id'));
-
-    });
-
-
-}
+});
--- js/detail-video.js
+++ js/detail-video.js
@@ -7,7 +7,6 @@ Jn.setData = function(data){
     }
 };
 
-
 function renderData(content){
     var data = content;
     var htmlStr = '<header><h1 class="title">' + data.title + '</h1>';
@@ -18,7 +17,7 @@ function renderData(content){
     htmlStr += '<div class="video-excerpt"><p>' + data.excerpt + '</p></div></header>';
     if (data.recomendVideos.length > 0) {
         htmlStr += '<section class="header-bottomline3"></section><section class="gamelist"><h3>视频推荐</h3> <ul> ';
-        $.each(data.recomendVideos, function (index, item) {//alert(1);
+        $.each(data.recomendVideos, function (index, item) {
             htmlStr += '<li class="clearFix"> <img class="fl" src="' + item.thumbnail + '"/>';
             htmlStr += ' <div class="fl gamelist-into"> <P class="gamelist-into-title">' + item.title + '</P> ';
             htmlStr += '<p class="gamelist-into-text">' + item.excerpt + '</p></div> ';
@@ -29,42 +28,20 @@ function renderData(content){
     $('#detail').html(htmlStr);
 }
 
-
-/*
-window.onload = window.onresize =function () {
+$(function(){
     document.documentElement.style.fontSize = document.documentElement.clientWidth / 320 * 20 + 'px';
-    $(function () {
-        $.get('data/video-detail.json', function (res) {
-            if (res.code == 10000) {
-                var data = res.data;
-                var htmlStr = '<header><h1 class="title">' + data.title + '</h1>';
-                htmlStr += '<div class="video-meta"><span class="date">' + data.time + '</span><span class="author color-blue ml5">';
-                htmlStr += data.author + '</span>';
-                htmlStr += '<span class="views">' + data.views + '</span></div>';
-                htmlStr += '<div class="player">' + data.content + '</div>';
-                htmlStr += '<div class="video-excerpt"><p>' + data.excerpt + '</p></div></header>';
-                if (data.recomendVideos.length > 0) {
-                    htmlStr += '<section class="header-bottomline3"></section><section class="gamelist"><h3>视频推荐</h3> <ul> ';
-                    $.each(data.recomendVideos, function (index, item) {//alert(1);
-                        htmlStr += '<li class="clearFix video-rec" data-type="'+item.articleType+'" data-id="'+item.extra+'"><img class="fl" src="' + item.thumbnail + '"/>';
-                        htmlStr += '<p class="gamelist-into-title">' + item.title + '</p>';
-                        htmlStr += '<p class="gamelist-into-text">' + item.excerpt + '</p></div> ';
-                        htmlStr += '<span class="tag" style="color:' + item.tagColor + ';border-color:' + item.tagColor + ';">' + item.tagName + '</span></li>';
-                    });
-                    htmlStr += '</ul> </section>';
-                }
-                $('#detail').html(htmlStr);
-            }
-        });
-
-        $(document).on('click', '.video-rec', function (e) {
-            e.preventDefault();
-            try{
-                Jnapp.jn_related($(this).data('type'), $(this).data('id'));
-            }catch(e){
-
-            }
-        });
-
+    $.get('data/video-detail.json', function (res) {
+        //if (res.code == 10000) {
+            renderData(res.content);
+        //}
     });
-}*/
+
+    $(document).on('click', '.video-rec', function (e) {
+        e.preventDefault();
+        try{
+            Jnapp.jn_related($(this).data('type'), $(this).data('id'));
+        }catch(e){
+
+        }
+    });
+});
