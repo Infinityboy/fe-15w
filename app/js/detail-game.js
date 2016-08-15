0c5925ee84f0f0a379884654638856575f9b1dd6
liangqi <liangqi000@gmail.com>
2016-08-15 20:25:01 +0800

    调整直播平台拍板问题

--- app/js/detail-game.js
+++ app/js/detail-game.js
@@ -298,11 +298,11 @@ function renderReviews(reviewsData, type) {
 
 
 $(function () {
-    $.get('data/living.json', function (res) {
-        if (res.code == 10000) {
-            renderData(res.data);
-        }
-    });
+    // $.get('data/living.json', function (res) {
+    //     if (res.code == 10000) {
+    //         renderData(res.data);
+    //     }
+    // });
 
     //观看直播
     $(document).on('click', '.deck>a', function (e) {
