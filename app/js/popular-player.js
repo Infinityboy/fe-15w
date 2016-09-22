4d7eeb30e9d88e7273191d5eda7c461624687051
duzhicai <duzhicai@15w.com>
2016-09-22 12:31:43 +0800

    popular

--- app/js/popular-player.js
+++ app/js/popular-player.js
@@ -21,25 +21,28 @@ function renderData(data){
         htmlStr+= '<p>距本期结束还有<em class="sale-time-describe">'+data.timeleft+'</em>天</p>';
         htmlStr+= '<p>榜单每五分钟刷新一次</p></div></div>';
         htmlStr+= '<div class="most-popular-players">';
-        htmlStr+= '<div class="popular-box" data-id="'+data.list[1].dataId+'" data-type="'+data.list[1].articleType+'">';
+        htmlStr+= '<a class="popular-box-skip" data-id="'+data.list[1].dataId+'" data-type="'+data.list[1].articleType+'">';
+        htmlStr+= '<div class="popular-box">';
         htmlStr+= '<div class="player-img"><img src="'+data.list[1].thumbnail+'" alt=""/></div>';
         htmlStr+= '<div class="player-sec-box">';
         htmlStr+= '<div class="player-sec-icon"><img src="images/Second_2x.png.png" alt=""/></div>';
         htmlStr+= '<span class="player-pact">'+data.list[1].sname+'</span>';
-        htmlStr+= '<span>'+data.list[1].goods+'</span></div></div>';
-        htmlStr+= '<div class="popular-box first" data-id="'+data.list[0].dataId+'" data-type="'+data.list[0].articleType+'">';
+        htmlStr+= '<span>'+data.list[1].goods+'</span></div></div></a>';
+        htmlStr+= '<a class="popular-box-skip" data-id="'+data.list[0].dataId+'" data-type="'+data.list[0].articleType+'">';
+        htmlStr+= '<div class="popular-box first">';
         htmlStr+= '<div class="player-img-first"><img src="'+data.list[0].thumbnail+'" alt=""/></div>';
         htmlStr+= '<div class="player-sec-box">';
         htmlStr+= '<div class="player-sec-icon"><img src="images/first_2x.png.png" alt=""/></div>';
         htmlStr+= '<span class="player-pact">'+data.list[0].sname+'</span>';
-        htmlStr+= '<span>'+data.list[0].goods+'</span></div></div>';
-        htmlStr+= '<div class="popular-box" data-id="'+data.list[2].dataId+'" data-type="'+data.list[2].articleType+'">';
+        htmlStr+= '<span>'+data.list[0].goods+'</span></div></div></a>';
+        htmlStr+= '<a class="popular-box-skip"data-id="'+data.list[2].dataId+'" data-type="'+data.list[2].articleType+'">';
+        htmlStr+= '<div class="popular-box">';
         htmlStr+= '<div class="player-img"><img src="'+data.list[2].thumbnail+'" alt=""/></div>';
         htmlStr+= '<div class="player-sec-box">';
         htmlStr+= '<div class="player-sec-icon"><img src="images/Third_2x.png.png" alt=""/></div>';
         htmlStr+= '<span class="player-pact">'+data.list[2].sname+'</span>';
         htmlStr+= '<span>'+data.list[2].goods+'</span>';
-        htmlStr+= '</div></div></div></div></div></section>';
+        htmlStr+= '</div></div></div></div></div></a></section>';
         htmlStr+= '<section class="popular-players" >';
         if(data.list.length>0){
             $.each(data.list,function(index,item){
@@ -48,11 +51,12 @@ function renderData(data){
                 }else{
                     rank = item.ranking;
                 }if(index>=3){
-                    htmlStr+= '<div class="popular-players-list" data-id="'+item.dataId+'" data-type="'+item.articleType+'">';
+                    htmlStr+= '<a class="popular-skip" data-id="'+item.dataId+'" data-type="'+item.articleType+'">';
+                    htmlStr+= '<div class="popular-players-list">';
                     htmlStr+= '<div class="rank" ><span>'+rank+'</span></div>';
                     htmlStr+= '<div class="rank-head"><img src="'+item.thumbnail+'" alt=""/></div>';
                     htmlStr+= '<div class="players-list-pact"><span>'+item.sname+'</span></div>';
-                    htmlStr+= '<div class="grades"><span>'+item.goods+'</span></div></div>';
+                    htmlStr+= '<div class="grades"><span>'+item.goods+'</span></div></div></a>';
                 }
 
             });
@@ -66,18 +70,18 @@ function renderData(data){
 
 $(function(){
 
-     $.ajax({
-         url: 'http://api.15w.com/client/app/jn/v1_3/game/ranking?typeId=player',
-         type: "GET",
-         dataType: 'jsonp',
-         success: function (res) {
-             renderData(res.data);
-         },
-         error: function (err) {
-             alert('失败:' + err);
-         }
-     });
-    $(document).on('click','.popular-box',function(e){
+     //$.ajax({
+     //    url: 'http://api.15w.com/client/app/jn/v1_3/game/ranking?typeId=player',
+     //    type: "GET",
+     //    dataType: 'jsonp',
+     //    success: function (res) {
+     //        renderData(res.data);
+     //    },
+     //    error: function (err) {
+     //        alert('失败:' + err);
+     //    }
+     //});
+    $(document).on('click','.popular-box-skip',function(e){
         e.preventDefault();
         try{
             var stringType,
@@ -94,7 +98,7 @@ $(function(){
         }
     });
 
-    $(document).on('click','.popular-players-list',function(e){
+    $(document).on('click','.popular-skip',function(e){
         e.preventDefault();
         try{
             var stringType,
