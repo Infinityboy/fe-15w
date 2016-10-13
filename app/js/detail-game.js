3e02426f0cd4e04d7743facea601da217cd9ffcd
duzhicai <duzhicai@15w.com>
2016-10-13 18:18:31 +0800

    detail-game

--- app/js/detail-game.js
+++ app/js/detail-game.js
@@ -346,7 +346,7 @@ function renderData(data) {
 	$('#box').html(htmlStr);
 
 	try{
-		var headerHeight = $(window).height();
+		var headerHeight = $('#header-h').height();
 		alert(headerHeight);
 		Jnapp.jn_sendHeaderHeight(headerHeight+'');
 	}catch (e){
@@ -446,11 +446,11 @@ function renderReviews(reviewsData, type) {
 
 $(function () {
 
-	$.get('data/living.json', function (res) {
-	    if (res.code == 10000) {
-	        renderData(res.data);
-	    }
-	});
+	//$.get('data/living.json', function (res) {
+	//    if (res.code == 10000) {
+	//        renderData(res.data);
+	//    }
+	//});
 
 	//观看直播
 	$(document).on('click', '.deck>a', function (e) {
